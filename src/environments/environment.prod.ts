d772f7ab0614151cdc1fdaba7a55c6175b56a275
sheezan-yo <khansheezan30@gmail.com>
2025-07-09 20:02:33 +0530

    updated backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     "production": true,
-    // "url": "https://angularecom.onrender.com/api"
-    url: process.env['NG_APP_BASE_URL']
+    "url": "https://angularecom.onrender.com/api"
+    // url: process.env['NG_APP_BASE_URL']
 };
